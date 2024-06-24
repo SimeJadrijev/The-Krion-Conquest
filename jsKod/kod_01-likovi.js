eb0c30132f516c639f192db33d498e3ae32abafc
SimeJadrijev <sime.jadrijev@gmail.com>
2024-06-24 11:25:30 +0200

    initial setup of Character and Francesca classes

--- jsKod/kod_01-likovi.js
+++ jsKod/kod_01-likovi.js
@@ -10,22 +10,86 @@
 
 // ovdje pišete svoje klase
 
-class Racoon extends Sprite {
-    constructor(x, y, layer) {
-        super(x, y, 60, 60);
-        this.frame_sets = {
-            "up": [1],
-            "walk-up": [1],
-            "right": [1],
-            "walk-right": [2, 3, 4, 5, 6, 7, 8],
-            "down": [1],
-            "walk-down": [1],
-            "left": [11],
-            "walk-left": [12, 13, 14, 15, 16, 17, 18]
-        };
+class Character extends Sprite {
+  constructor(x, y, layer) {
+    super(x + 4, y + 4, 64 - 8, 64 - 8);
 
-        this.layer = layer;
-        this.visible = true; //tek kad se postavi layer
+    this.frame_sets = {};
 
+    this.layer = layer;
+    this.visible = true;
+  }
+
+  jump(h = 50) {
+    if (!this.jumping) {
+      this.jumping = true;
+      this.velocity_y -= h;
     }
+  }
+}
+
+class Francesca extends Character {
+  #lives;
+  constructor(layer) {
+    super(0, 0, layer);
+    this.frame_sets = {
+      up: [31],
+      "walk-up": [31],
+      right: [31],
+      "walk-right": [32, 33, 34],
+      down: [31],
+      "walk-down": [31],
+      left: [38],
+      "walk-left": [37, 36, 35],
+    };
+
+    this.shoots = false;
+    this.points = 0;
+    this.#lives = 1;
+    this.boxTouch = false;
+    this.activeMissiles = 0;
+
+    this.direction = 90;
+  }
+
+  get lives() {
+    return this.#lives;
+  }
+
+  set lives(v) {
+    if (v <= 0) {
+      btnStop_click();
+    } else {
+      this.#lives = v;
+    }
+  }
+
+  moveRight() {
+    let a = arguments.length;
+    switch (a) {
+      case 0:
+        super.moveRight();
+        break;
+      case 1:
+        this.direction = 90;
+        this.velocity_x += arguments[0];
+      default:
+        break;
+    }
+  }
+
+  moveLeft() {
+    let a = arguments.length;
+    switch (a) {
+      case 0:
+        super.moveLeft();
+
+        break;
+      case 1:
+        this.direction = 270;
+        this.velocity_x -= arguments[0];
+      default:
+        break;
+    }
+  }
 }
