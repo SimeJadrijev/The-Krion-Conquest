8bea51587a5c707129b879fd3c71a3aae4b043c5
SimeJadrijev <sime.jadrijev@gmail.com>
2024-06-25 18:12:22 +0200

    enabled moving for francesca

--- jsKod/kod_03-logikaIgre.js
+++ jsKod/kod_03-logikaIgre.js
@@ -15,7 +15,35 @@
  * Promjena stanja likova - interakcije
  */
 function update_main() {
-  // if (GAME.activeWorldMap.name == "Level one") vjezbe10();
+  if (GAME.activeWorldMap.name == "Level one") firstLevel();
 
   GAME.update();
 }
+
+const firstLevel = () => {
+  handleInput();
+};
+
+const handleInput = () => {
+  let activeInput = null;
+
+  if (SENSING.left.active || SENSING.keyA.active) activeInput = "left";
+  if (SENSING.right.active || SENSING.keyD.active) activeInput = "right";
+  if (SENSING.up.active || SENSING.keyW.active) activeInput = "up";
+  if (SENSING.space.active) activeInput = "space";
+
+  switch (activeInput) {
+    case "left":
+      Postavke.francesca.moveLeft(2);
+      break;
+    case "right":
+      Postavke.francesca.moveRight(2);
+      break;
+    case "up":
+      Postavke.francesca.jump();
+      break;
+
+    default:
+      break;
+  }
+};
